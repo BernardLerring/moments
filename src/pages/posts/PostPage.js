116bb3a235a292f31cce63f4aa2ab849e07a733d
Tim <orcatim@hotmail.com>
2022-09-04 19:55:42 +0000

    add posts to profile page

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -12,6 +12,7 @@ import Comment from "../comments/Comment";
 
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
 import InfiniteScroll from "react-infinite-scroll-component";
 import Asset from "../../components/Asset";
 import { fetchMoreData } from "../../utils/utils";
@@ -59,20 +60,20 @@ function PostPage() {
             "Comments"
           ) : null}
           {comments.results.length ? (
-            <InfiniteScroll 
-            children ={
-              comments.results.map((comment) => (
-              <Comment key={comment.id} {...comment}
-              setPost={setPost} 
-              setComments={setComments} />
-            ))
-            }
-            dataLength={comments.results.length}
-                loader={<Asset spinner />}
-                hasMore={!!comments.next}
-                next={() => fetchMoreData(comments, setComments)}
-
-              /> 
+            <InfiniteScroll
+              children={comments.results.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  {...comment}
+                  setPost={setPost}
+                  setComments={setComments}
+                />
+              ))}
+              dataLength={comments.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!comments.next}
+              next={() => fetchMoreData(comments, setComments)}
+            />
           ) : currentUser ? (
             <span>No comments yet, be the first to comment!</span>
           ) : (
